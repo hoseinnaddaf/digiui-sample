6d395b3ed34b389a56ac835c5dc07d2decc27941
hoseinnaddaf <naddaf.moghaddam@yahoo.com>
2023-01-19 15:20:04 +0330

    add banners

--- lib/UI/HomePage.dart
+++ lib/UI/HomePage.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:ffi';
 
+import 'package:digiui_sample/models/Banner.dart';
 import 'package:digiui_sample/models/SpecialOfeerModel.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,7 @@ class _HomePageState extends State<HomePage> {
 
    late Future<List<Slide>>  imageSlider_future ;
    late Future<List<SpecialOfferModel>>  specialOffers_future ;
+   late Future<List<BannerData>>  banners_future ;
 
    PageController pageController = PageController() ;
 
@@ -31,6 +33,7 @@ class _HomePageState extends State<HomePage> {
 
     imageSlider_future =  getimageSliderData() ;
     specialOffers_future = getSpecialOffersData() ;
+    banners_future = getBannersData() ;
   }
 
   Future<List<Slide>> getimageSliderData() async {
@@ -62,7 +65,6 @@ class _HomePageState extends State<HomePage> {
        var datas =  json.decode(respone.data) ;
        datas.forEach( (var data)
        {
-
          specialOffers.add(SpecialOfferModel(
                                              data["product_name"],
                                              data["price"],
@@ -71,9 +73,27 @@ class _HomePageState extends State<HomePage> {
                                              data["imageUrl"])) ;}) ;
      }
 
-     print("array has : "+ specialOffers.length.toString() + " element") ;
-
      return specialOffers ;
+   }
+
+   Future<List<BannerData>> getBannersData() async {
+
+     List<BannerData> banners =[] ;
+
+     var respone =  await Dio().get("http://mhnaddaf.ir/digiui_api/getBannersData.php") ;
+     if(respone.statusCode== 200)
+     {
+       var datas =  json.decode(respone.data) ;
+       datas.forEach( (var data)
+       {
+         banners.add(BannerData( data["imageUrl"])) ;
+       }
+       ) ;
+
+     }
+
+     print("datas size : "+ banners.length.toString()) ;
+     return banners ;
    }
 
   @override
@@ -89,136 +109,206 @@ class _HomePageState extends State<HomePage> {
             icon: Icon(Icons.shopping_cart_outlined)),
       ),
 
-      body:Container(
-        child: Column(
-          children: [
-            Container(
-              height: 200,
+      body:SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                child: FutureBuilder<List<Slide>>(
+                    future: imageSlider_future,
+                    builder: (context ,  snappshot){
 
-              child: FutureBuilder<List<Slide>>(
-                  future: imageSlider_future,
-                  builder: (context ,  snappshot){
-
-                    if(snappshot.hasData)
-                    {
-                      List<Slide>? models =  snappshot.data ;
-
-                      return Stack(
-                        alignment: Alignment.bottomCenter,
-                        children: [
-                          PageView.builder(
-                              controller: pageController,
-                              allowImplicitScrolling: true,
-                              itemCount: models?.length,
-                              itemBuilder:(context , position)
-                              {
-                                return PageViewItems(models![position]) ;
-                              }
-                          ) ,
-                          Padding(
-                              padding:EdgeInsets.only(bottom: 5) ,
-                              child:SmoothPageIndicator(
-                                count: models!.length,
-                                controller:pageController ,
-                                effect: ExpandingDotsEffect(
-                                    dotWidth: 10 ,
-                                    dotHeight: 10 ,
-                                    spacing: 3 ,
-                                    dotColor: Colors.white ,
-                                    activeDotColor: Colors.black
-                                  ),
-                                onDotClicked: (index)=>
-                                    pageController.animateToPage(
-                                        index, duration: Duration(milliseconds: 300), curve: Curves.bounceOut)
-                              ),
-                          ) ,
-
-                        ],
-                      );
-                    }
-                    else
-                    {
-                      return Center(
-                        child: JumpingDotsProgressIndicator(
-                        fontSize: 60,
-                          dotSpacing: 5,
-                        ),
-                      ) ;
-                    }
-                  },
-              ),
-            ) ,
-            Container(
-              height: 10,
-            )
-            ,
-            Container(
-              height: 300,
-              color: Color(0xFFef4758),
-              child: FutureBuilder<List<SpecialOfferModel>>(
-                  future: specialOffers_future,
-                  builder: (context ,  snapshot){
-                      if(snapshot.hasData)
+                      if(snappshot.hasData)
                       {
-                        List<SpecialOfferModel>? special_data =  snapshot.data ;
+                        List<Slide>? models =  snappshot.data ;
 
-                        return ListView.builder(
-                            reverse: true,
-                            shrinkWrap: true,
-                            scrollDirection: Axis.horizontal,
-                            itemCount:special_data!.length ,
-                            itemBuilder: (context ,  position)
-                              {
-                                if(position ==0 )
-                                  {
-                                    return Container(
-                                      height: 300,
-                                      width: 150,
-                                      child: Column(
-                                        children: [
-                                          Padding(
-                                            padding: const EdgeInsets.only(top: 15 , left: 10 , right: 10),
-                                            child: Image.asset("assets/images/specialoffer.png",  height: 230 ,),
-                                          ),
-                                          Padding( padding: const EdgeInsets.only(bottom: 5),
-                                            child:Expanded(
-                                              child: OutlinedButton(onPressed: (){},
-                                                  style:OutlinedButton.styleFrom(
-                                                    side: BorderSide(color: Colors.white) ,
+                        return Stack(
+                          alignment: Alignment.bottomCenter,
+                          children: [
+                            PageView.builder(
+                                controller: pageController,
+                                allowImplicitScrolling: true,
+                                itemCount: models?.length,
+                                itemBuilder:(context , position)
+                                {
+                                  return PageViewItems(models![position]) ;
+                                }
+                            ) ,
+                            Padding(
+                                padding:EdgeInsets.only(bottom: 5) ,
+                                child:SmoothPageIndicator(
+                                  count: models!.length,
+                                  controller:pageController ,
+                                  effect: ExpandingDotsEffect(
+                                      dotWidth: 10 ,
+                                      dotHeight: 10 ,
+                                      spacing: 3 ,
+                                      dotColor: Colors.white ,
+                                      activeDotColor: Colors.black
+                                    ),
+                                  onDotClicked: (index)=>
+                                      pageController.animateToPage(
+                                          index, duration: Duration(milliseconds: 300), curve: Curves.bounceOut)
+                                ),
+                            ) ,
 
-                                                  ),
-                                                  child: Text("مشاهده همه" , style: TextStyle(color: Colors.white),)
-
-                                              ),
-
-                                            )
-                                          )
-
-                                        ],
-                                      ),
-                                    );
-                                  }
-                                else
-                                  {
-                                    return SpecialOfferItem(special_data[position-1]) ;
-                                  }
-                              }
-
-                        ) ;
+                          ],
+                        );
                       }
                       else
                       {
                         return Center(
                           child: JumpingDotsProgressIndicator(
-                            fontSize: 60,
+                          fontSize: 60,
                             dotSpacing: 5,
                           ),
                         ) ;
                       }
-                  }
-              ),
-            )
-          ],
+                    },
+                ),
+              ) ,
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Container(
+                  height: 300,
+                  color: Color(0xFFef4758),
+                  child: FutureBuilder<List<SpecialOfferModel>>(
+                      future: specialOffers_future,
+                      builder: (context ,  snapshot){
+                          if(snapshot.hasData)
+                          {
+                            List<SpecialOfferModel>? special_data =  snapshot.data ;
+
+                            return ListView.builder(
+                                reverse: true,
+                                shrinkWrap: true,
+                                scrollDirection: Axis.horizontal,
+                                itemCount:special_data!.length ,
+                                itemBuilder: (context ,  position)
+                                  {
+                                    if(position ==0 )
+                                      {
+                                        return Container(
+                                          height: 300,
+                                          width: 150,
+                                          child: Column(
+                                            children: [
+                                              Padding(
+                                                padding: const EdgeInsets.only(top: 15 , left: 10 , right: 10),
+                                                child: Image.asset("assets/images/specialoffer.png",  height: 230 ,),
+                                              ),
+                                              Padding( padding: const EdgeInsets.only(bottom: 5),
+                                                child:Expanded(
+                                                  child: OutlinedButton(onPressed: (){},
+                                                      style:OutlinedButton.styleFrom(
+                                                        side: BorderSide(color: Colors.white) ,
+
+                                                      ),
+                                                      child: Text("مشاهده همه" , style: TextStyle(color: Colors.white),)
+
+                                                  ),
+
+                                                )
+                                              )
+
+                                            ],
+                                          ),
+                                        );
+                                      }
+                                    else
+                                      {
+                                        return SpecialOfferItem(special_data[position-1]) ;
+                                      }
+                                  }
+
+                            ) ;
+                          }
+                          else
+                          {
+                            return Center(
+                              child: JumpingDotsProgressIndicator(
+                                fontSize: 60,
+                                dotSpacing: 5,
+                              ),
+                            ) ;
+                          }
+                      }
+                  ),
+                ),
+              ) ,
+              Padding(
+                padding: EdgeInsets.only(top: 5),
+                child: Container(
+                    width: double.infinity,
+                    child: FutureBuilder<List<BannerData>>(
+                      future: banners_future,
+                      builder: (context , snapshot)
+                      {
+                       if(snapshot.hasData)
+                         {
+                           List<BannerData>? model = snapshot.data ;
+
+                           return Column(
+                             children: [
+                               Padding(
+                                 padding: const EdgeInsets.only(top: 5),
+                                 child: Row(
+                                   mainAxisAlignment: MainAxisAlignment.spaceEvenly
+                                   ,children: [
+                                    Container(
+                                      height: 150,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.all(Radius.circular(15)),
+                                          child: Image.network(model![0].imageUrl, fit: BoxFit.fill, width: 190,)),
+                                    ) ,
+                                    Container(
+                                      height: 150,
+                                      child: ClipRRect(
+                                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                                          child: Image.network(model![1].imageUrl, fit: BoxFit.fill, width: 190,)),
+                                    )
+                                  ],
+                                 ),
+                               ) ,
+                               Padding(
+                                 padding: const EdgeInsets.only(top: 5),
+                                 child: Row(
+                                   mainAxisAlignment: MainAxisAlignment.spaceEvenly
+                                   ,children: [
+                                   Container(
+                                     height: 150,
+                                     child: ClipRRect(
+                                         borderRadius: BorderRadius.all(Radius.circular(15)),
+                                         child: Image.network(model![2].imageUrl, fit: BoxFit.fill, width: 190,)),
+                                   ) ,
+                                   Container(
+                                     height: 150,
+                                     child: ClipRRect(
+                                         borderRadius: BorderRadius.all(Radius.circular(15)),
+                                         child: Image.network(model![3].imageUrl, fit: BoxFit.fill, width: 190,)),
+                                   )
+                                 ],
+                                 ),
+                               )
+                             ],
+                           );
+                         }
+                       else
+                         {
+                           return Center(
+                             child: JumpingDotsProgressIndicator(
+                               fontSize: 60,
+                               dotSpacing: 5,
+                             ),
+                           ) ;
+                         }
+                      },
+                    ),
+                ),
+              )
+            ],
+          ),
         ),
       )
       ,
@@ -253,7 +343,7 @@ class _HomePageState extends State<HomePage> {
                 children: [
                   Padding(
                   padding:EdgeInsets.all(10),
-                  child :Image.network(specialOfferModel.imageUrl  , height: 150 ,fit:BoxFit.fill,)
+                  child :Image.network(specialOfferModel.imageUrl  , height: 160 ,fit:BoxFit.fill,)
                   ) ,
                   Padding(padding: EdgeInsets.only(top: 5) ,
                   child: Text(specialOfferModel.product_name , ),
---
+++ lib/models/Banner.dart
@@ -0,0 +1,13 @@
+class BannerData{
+
+  String _imageUrl ;
+
+  BannerData( this._imageUrl);
+
+  String get imageUrl => _imageUrl;
+
+  set imageUrl(String value) {
+    _imageUrl = value;
+  }
+
+}
